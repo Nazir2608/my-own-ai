d44f01d8e8abe51ec6873f75e75afa2cf770c231
Md Nazir <mdnazir2608@gmail.com>
2026-04-19 14:38:10 +0530

    refactored do filter incoming and outgoing request

--- src/main/java/com/nazir/myownai/config/RequestLoggingFilter.java
+++ src/main/java/com/nazir/myownai/config/RequestLoggingFilter.java
@@ -1,6 +1,10 @@
 package com.nazir.myownai.config;
 
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -12,14 +16,14 @@ public class RequestLoggingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        
-        System.out.println("📥 " + req.getMethod() + " " + req.getRequestURI());
-        
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        System.out.println("INCOMING " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+
         chain.doFilter(request, response);
-        
-        System.out.println("📤 " + req.getMethod() + " " + req.getRequestURI() + " → " + res.getStatus());
+
+        System.out.println("OUTGOING " + httpRequest.getMethod() + " " + httpRequest.getRequestURI() + " STATUS " + httpResponse.getStatus());
     }
 }
